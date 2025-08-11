09d2f59b19c89c1fe3704f334e4a82cb28646e54
xitrum2708 <hongfxhp@gmail.com>
2025-08-11 17:12:19 +0700

    Add view for search qr and barcode

--- Models/UVASSY/VM/UVASSY_PPRODUCT_HISTORY_ViewModel.cs
+++ Models/UVASSY/VM/UVASSY_PPRODUCT_HISTORY_ViewModel.cs
@@ -4,6 +4,8 @@ namespace MESWebDev.Models.UVASSY.VM
 {
     public class UVASSY_PPRODUCT_HISTORY_ViewModel
     {
-        public DataTable SearchResult { get; set; }        
+        public DataTable SearchResult { get; set; } 
+        public DataTable AttachedList { get; set; }       
+
     }
 }
---
+++ Services/UVASSY_PPRODUCT_HISTORY.cs
@@ -0,0 +1,46 @@
+﻿using MESWebDev.Common;
+using MESWebDev.Data;
+using System.Data;
+
+namespace MESWebDev.Services
+{
+    public class UVASSY_PPRODUCT_HISTORY:IUVASSY_PPRODUCT_HISTORY
+    {
+        private readonly AppDbContext _context;
+        private readonly Procedure _proc;
+              
+
+        public UVASSY_PPRODUCT_HISTORY(AppDbContext context)
+        {
+            _context = context;
+            _proc = new Procedure(context); ;
+        }
+
+        public Task<DataTable> GetAttachedFile(int Id)
+        {
+            return _proc.Proc_GetDatatable("sp_Attach_GetById", new Dictionary<string, object>
+            {
+                { "@Id", Id }
+            });
+        }
+
+        public Task<DataTable> GetAttachedList(int processID, string qrcode)
+        {
+            return _proc.Proc_GetDatatable("sp_Attach_ListByProcQr", new Dictionary<string, object>
+            {
+                { "@processID", processID },
+                { "@Qrcode", qrcode }
+            });
+        }
+
+        public Task<DataTable> SearchResult(string search)
+        {
+            return _proc.Proc_GetDatatable("sp_UVASSY_PPRODUCT_HISTORY_SearchCascade", new Dictionary<string, object>
+            {
+                { "@key", search }
+            });
+        }
+
+
+    }
+}
