d9a51132b536931dd7c644b76cad4edc40f918af
xitrum2708 <hongfxhp@gmail.com>
2025-08-11 14:30:03 +0700

    add search

---
+++ Models/UVASSY/VM/UVASSY_PPRODUCT_HISTORY_ViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace MESWebDev.Models.UVASSY.VM
+{
+    public class UVASSY_PPRODUCT_HISTORY_ViewModel
+    {
+        public DataTable SearchResult { get; set; }        
+    }
+}
