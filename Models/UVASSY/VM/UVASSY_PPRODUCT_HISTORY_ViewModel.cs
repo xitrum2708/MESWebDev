04c365ca3875b20b96b49bd61c9a152b3913339a
xitrum2708 <hongfxhp@gmail.com>
2025-08-14 14:41:57 +0700

    Add change

--- Models/UVASSY/VM/UVASSY_PPRODUCT_HISTORY_ViewModel.cs
+++ Models/UVASSY/VM/UVASSY_PPRODUCT_HISTORY_ViewModel.cs
@@ -4,12 +4,8 @@ namespace MESWebDev.Models.UVASSY.VM
 {
     public class UVASSY_PPRODUCT_HISTORY_ViewModel
     {
-<<<<<<< HEAD
-        public DataTable SearchResult { get; set; }        
-=======
         public DataTable SearchResult { get; set; } 
-        public DataTable AttachedList { get; set; }       
+        public DataTable AttachedList { get; set; }  
 
->>>>>>> 792b709426c12fba41f06e304d1266c05f739871
     }
 }
